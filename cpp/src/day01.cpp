caf11ae2cf0b3ca69f9478b4178df19b6ae347e0
Thomas Peiselt <pi@kulturguerilla.org>
2021-12-01 23:06:43 +0100

    cpp, not elegant

---
+++ cpp/src/day01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+int part1() {
+  auto c = int { 0 };
+  auto next = int { 0 }; 
+  auto prev = 1l<<62; 
+  while(std::cin >> next) {
+    c += next > prev;
+    prev = next;
+  }
+  return c;
+}
+
+int part2() {
+  std::deque<int> inputs;
+  auto c = int { 0 };
+  int next;
+  while(std::cin >> next) {
+    if(inputs.size() > 2) {
+      c += next > inputs[0];
+      inputs.pop_front();
+    }
+    inputs.push_back(next);
+  }
+  return c;
+}
+
+int main() {
+  std::cout << part2() << '\n';
+}
+
+
