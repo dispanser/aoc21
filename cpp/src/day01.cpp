82f4375bbf32bae5b1eeb3aced6aae168c28dff3
Thomas Peiselt <pi@kulturguerilla.org>
2021-12-04 16:23:36 +0100

    Day03, part 2.
    not elegant

--- cpp/src/day01.cpp
+++ cpp/src/day01.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <deque>
 
 int part1() {
--- hs/src/Day03.hs
+++ hs/src/Day03.hs
@@ -19,16 +19,46 @@ value :: [Bool] -> Int
 value [] = 0
 value (x:xs) = (if x then 1 else 0) + 2 * value xs
 
+-- part 1: 2972336
 powerConsumption :: [String] -> Int
 powerConsumption ts = gamma * epsilon
     where
         threshold = length ts `div` 2
-        counts  = (>threshold) <$> countAll ts
-        gamma   = value $ reverse counts
-        epsilon = value $ reverse $ not <$> counts
+        counts  = reverse $ (>threshold) <$> countAll ts
+        gamma   = value $ counts
+        epsilon = value $ not <$> counts
+
+checkBit :: Bool -> [Char] -> Char
+checkBit _ [c] = c
+checkBit m cs  =
+    let ones   = count (== '1') cs
+        zeroes = length cs - ones
+    in if ones >= zeroes && m || ones < zeroes && (not m)
+        then '1'
+        else '0'
+
+deriveRating :: Bool -> [String] -> String
+deriveRating m ts
+    | null (head ts) = []
+    | otherwise      =
+        let firstColumn      = head <$> ts
+            bit              = checkBit m firstColumn
+            remainingNumbers = filter (\x -> head x == bit) ts
+            tails            = tail <$> remainingNumbers
+        in bit : deriveRating m tails
+
+valueString :: String -> Int
+valueString ts = value $ reverse ((== '1') <$> ts)
+
+-- part 2: 3368358
+liveSupportRating :: [String] -> Int
+liveSupportRating ts = oxygen * scrubber
+    where
+        oxygen   = valueString $ deriveRating True ts
+        scrubber = valueString $ deriveRating False ts
 
 main :: IO ()
 main = do
   inputs <- getContents
-  print $ countAll $ lines inputs
   print $ powerConsumption $ lines inputs
+  print $ liveSupportRating $ lines inputs
