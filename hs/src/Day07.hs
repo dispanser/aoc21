c5c240ba146d7f19ff00842cce8381b9efbdb1d8
Thomas Peiselt <pi@kulturguerilla.org>
2021-12-07 19:50:05 +0100

    Day 7, parts 1 + 2.
    Part 2 solution is rather brute-forcey, but hey it's Tuesday; what do
    you expect?

---
+++ hs/src/Day07.hs
@@ -0,0 +1,30 @@
+import Data.List (sort, group)
+import Data.List.Extra (split)
+
+part1 :: [Int] -> Int
+part1 numbers =
+  let l      = length numbers
+      sorted = sort numbers
+      median = sorted !! (l `div` 2)
+  in sum $ abs . (median -) <$> sorted
+
+part2brute :: [Int] -> Int
+part2brute numbers =
+  let low    = minimum numbers
+      high   = maximum numbers
+      costSingle delta = (abs delta * succ (abs delta)) `div` 2
+      costAll n        = sum $ costSingle . (n-) <$> numbers
+  in  minimum $ costAll <$> [low .. high]
+
+main :: IO ()
+main = do
+  tokens <- split (== ',') <$> getContents
+  let numbers = read <$> tokens :: [Int]
+  let sumx = sum numbers
+  let num  = length numbers
+  print sumx
+  print num
+  print $ maximum numbers
+  print $ minimum numbers
+  print $ part1 numbers
+  print $ part2brute numbers
