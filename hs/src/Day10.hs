2939b8f7bcea461486857000f2eb9d7c37e95652
Thomas Peiselt <pi@kulturguerilla.org>
2021-12-12 07:37:27 +0100

    Day 10, part 2.

--- hs/src/Day10.hs
+++ hs/src/Day10.hs
@@ -1,26 +1,62 @@
+{-# LANGUAGE LambdaCase #-}
 module Day10 where
+
+import Foreign (callocArray0)
+import Data.Maybe (mapMaybe)
+import Data.List (sort)
+
+data LineAnalysis =
+    CorruptLine Int |
+    IncompleteLine String
 
 -- plan: recurse! pop off a matching closing delimiter; append
 -- an opening char when observed; mismatched closing gets a score
 -- end of char stream gets a 0
-part1LineScore :: String -> String -> Int
-part1LineScore es (c:cs)
-    | c `elem` ['(', '[', '{', '<'] = part1LineScore (c:es) cs
-part1LineScore ('[':es) (']':cs) = part1LineScore es cs -- matching []
-part1LineScore ('(':es) (')':cs) = part1LineScore es cs -- matching ()
-part1LineScore ('{':es) ('}':cs) = part1LineScore es cs -- matching {}
-part1LineScore ('<':es) ('>':cs) = part1LineScore es cs -- matching <>
-part1LineScore _ (')':_) = 3
-part1LineScore _ (']':_) = 57
-part1LineScore _ ('}':_) = 1197
-part1LineScore _ ('>':_) = 25137
-part1LineScore _ [] = 0   -- empty input, icomplete line
+lineAnalysis :: String -> String -> LineAnalysis
+lineAnalysis es (c:cs)
+    | c `elem` ['(', '[', '{', '<'] = lineAnalysis (c:es) cs
+lineAnalysis ('[':es) (']':cs) = lineAnalysis es cs -- matching []
+lineAnalysis ('(':es) (')':cs) = lineAnalysis es cs -- matching ()
+lineAnalysis ('{':es) ('}':cs) = lineAnalysis es cs -- matching {}
+lineAnalysis ('<':es) ('>':cs) = lineAnalysis es cs -- matching <>
+lineAnalysis _ (')':_) = CorruptLine 3
+lineAnalysis _ (']':_) = CorruptLine 57
+lineAnalysis _ ('}':_) = CorruptLine 1197
+lineAnalysis _ ('>':_) = CorruptLine 25137
+lineAnalysis expected  [] = IncompleteLine expected
 
+
+analyseLines :: [String] -> [LineAnalysis]
+analyseLines ls = lineAnalysis "" <$> ls
 
 part1 :: [String] -> Int
-part1 ls = sum $ part1LineScore "" <$> ls
+part1 ls =
+  let lsa        = analyseLines ls
+      lineScores = mapMaybe (\case
+              CorruptLine x    -> Just x
+              IncompleteLine _ -> Nothing)
+            lsa
+  in sum lineScores
+
+scoreIncomplete :: String -> Int
+scoreIncomplete [] = 0
+scoreIncomplete ('(':xs) = 1 + 5 * scoreIncomplete xs
+scoreIncomplete ('[':xs) = 2 + 5 * scoreIncomplete xs
+scoreIncomplete ('{':xs) = 3 + 5 * scoreIncomplete xs
+scoreIncomplete ('<':xs) = 4 + 5 * scoreIncomplete xs
+
+part2 :: [String] -> Int
+part2 ls =
+  let lsa        = analyseLines ls
+      lineScores = mapMaybe (\case
+              CorruptLine _    -> Nothing
+              IncompleteLine e -> Just . scoreIncomplete $ reverse e)
+            lsa
+      medianIdx  = length lineScores `div` 2
+  in sort lineScores !! medianIdx
 
 main :: IO ()
 main = do
   ls <- lines <$> getContents
   print $ part1 ls
+  print $ part2 ls
