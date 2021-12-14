e4a17758d7d498587c3df2bd4b5633fe2fda5d4f
Thomas Peiselt <pi@kulturguerilla.org>
2021-12-14 22:00:23 +0100

    Day 14, part 2.
    Replaces part 1 this time around, the new impl also solves part 1

--- hs/src/Day14.hs
+++ hs/src/Day14.hs
@@ -1,49 +1,71 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Day14 where
 
+import           Data.MemoTrie (memo, memo2)
 import           Control.Monad (mapM_)
 import           Data.Either (rights)
 import           Data.List (sort, group)
 import qualified Data.Map.Strict as M
-import           Data.Map.Strict ((!?))
+import           Data.Map.Strict ((!?), (!))
 import           Data.Maybe (fromMaybe)
 
 import Text.Parsec (char, many1, digit, string, spaces, parse, letter)
 import Text.Parsec.String (Parser)
 
-type RuleSet = M.Map (Char, Char) String
+type RuleSet = M.Map (Char, Char) Char
 
-applyRule :: RuleSet -> Char -> Char -> String
-applyRule rs c1 c2 = fromMaybe [c1, c2] $ rs !? (c1, c2)
+type Result = M.Map Char Int
 
-parseRule :: Parser ((Char, Char), String)
+parseRule :: Parser ((Char, Char), Char)
 parseRule = do
   c1 <- letter
   c2 <- letter
   _  <- spaces *> string "->" *> spaces
   r <- letter
-  return ((c1, c2), [r, c2])
+  return ((c1, c2), r)
 
 parseRules :: [String] -> RuleSet
 parseRules rs = M.fromList .rights $ parse parseRule "" <$> rs
 
-step :: RuleSet -> String -> String
-step rs inp = head inp : concat (zipWith (applyRule rs) inp $ tail inp)
+-- non-memoizing recursive implementation, but in contrast to
+-- part 1, with a signature that's suitable for memoization
+part2Rec :: RuleSet -> Int -> (Char, Char) -> Result
+part2Rec rs 0        ( _, c2) = M.singleton c2 1
+part2Rec rs count cs@(c1, c2) =
+  let inbetweenChar = rs ! cs
+  in M.unionWith (+) (part2Rec rs (count-1) (c1, inbetweenChar))
+                     (part2Rec rs (count-1) (inbetweenChar, c2))
 
-part1 :: RuleSet -> String -> Int
-part1 rs inp =
-  let iterations = iterate (step rs) inp
-      tenth      = sort $ iterations !! 10
-      chars      = length <$> group tenth
-  in maximum chars - minimum chars
+-- memo edition
+part2Rec' :: RuleSet -> Int -> (Char, Char) -> Result
+part2Rec' rs = part2Rec''
+  where part2Rec'' = memo2 (\ count cs@(c1, c2) ->
+              case count of
+                0 -> M.singleton c2 1
+                n ->
+                  let inbetweenChar = rs ! cs
+                  in M.unionWith (+) (part2Rec'' (count-1) (c1, inbetweenChar))
+                     (part2Rec'' (count-1) (inbetweenChar, c2))
+            )
 
+
+part2 :: RuleSet -> Int -> String -> Result
+part2 rs count inp =
+  let pairs          = inp `zip` tail inp
+      partialResults = part2Rec' rs count <$> pairs
+      charCounts     = foldl (M.unionWith (+)) (M.singleton (head inp) 1) partialResults
+  in charCounts
 
 main :: IO ()
 main = do
   ls <- lines <$> getContents
   let input = head ls
   let rules = parseRules (drop 2 ls)
-  let iterations = take 5 $ iterate (step rules) input
-  mapM_ print iterations
-  print $ part1 rules input
-
+  let resp1 = part2 rules 10 input
+  let countsp1 = snd <$> M.toList resp1
+  let res = part2 rules 40 input
+  let counts = snd <$> M.toList res
+  print (maximum countsp1 - minimum countsp1)
+  print (maximum counts - minimum counts)
 
